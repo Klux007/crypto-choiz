48d289473bbd06190e1b4c0e0251b7dfb227a77d
Klux007 <271939813+Klux007@users.noreply.github.com>
2026-09-15 16:27:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler';
+import AppNavigator from './src/navigation/AppNavigator';
+
+registerRootComponent(AppNavigator);
